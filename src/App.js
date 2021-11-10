73af835a5ba42e3f3e4ecebda760d89fad2f700c
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2021-11-10 18:35:11 -0300

    Adds BtnValues matrix, to be flat() and maped inside buttonBox component

--- src/App.js
+++ src/App.js
@@ -3,18 +3,35 @@ import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
 
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
 const App = () => {
   return (
     <Wrapper>
-      <Screen value="0" />
+      <Screen value="0"/>
       <ButtonBox>
-        <Button
-          className=""
-          value="0"
-          onClick={() => {
-            console.log("Button clicked!");
-          }}
-        />
+        {
+          // Flat() inserts all btn values inside a single array, map goes through them and returns a button with the required key, className, value and onClick properties
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                // Notice exception for the "=" operator to be a "equals" string
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
       </ButtonBox>
     </Wrapper>
   );
