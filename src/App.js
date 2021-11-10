c3532d395a73d6a0c91a94754548ea114d487a9a
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2021-11-10 20:23:37 -0300

    Adds casio styles and content

--- src/App.js
+++ src/App.js
@@ -141,6 +141,11 @@ const App = () => {
 
   return (
     <Wrapper>
+      <div className="casio">
+        <h3>CASIO</h3>
+        <p className="upper">ELETRONIC CALCULATOR</p>
+        <p>HL-815L</p>
+      </div>
       <Screen value={calc.num ? calc.num : calc.res} />
       <ButtonBox>
         {
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import MetaTags from 'react-meta-tags';
 
 import App from "./App";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <MetaTags>
+      <title>Calculator</title>
+    </MetaTags>
     <App />
   </React.StrictMode>,
   document.getElementById("root")
